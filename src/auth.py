4dc6f290caa8633d03ac9b749188e32e1e122e4b
quahjieren@gmail.com <quahjieren@gmail.com>
2018-11-10 15:30:43 +0800

    added register view to auth.py

--- src/auth.py
+++ src/auth.py
@@ -15,3 +15,30 @@ from src.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')  # creates instance 'bp' of Blueprint object. url_prefix is prepended to all URLS associated with the blueprint
 
+# writing the view code
+
+
+@bp.route('/register', methods=('GET', 'POST'))  # when request received to /auth/register, calls register view and return value as respponse
+def register():  # define register view function
+    if request.method() == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()  # fetch database and store in db
+        error = None
+
+        if not username:  # checks for case in which username is not defined
+            error = 'Username is required!'
+        elif not password:  # checks for case in which password is not defined
+            error = 'Password is required!'
+        elif db.execute('SELECT id FROM user WHERE username = %username', username=username).fetchone() is not None:  # checks for case in which user is already registered. returns one row from the query
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute('INSERT INTO user (username, password) VALUES (%username, %password)',username=username, password=generate_password_hash(password))
+            db.commit()  # saves changes to the db
+
+            return redirect(url_for('auth.login'))
+
+        flash(error)  # print error into the terminal if any
+
+    return render_template('auth.register.html')
